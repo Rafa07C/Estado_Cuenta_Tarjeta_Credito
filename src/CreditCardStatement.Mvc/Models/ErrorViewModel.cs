c3bdb2e03370e7ac05e7fbd996c35b8c8d8b7cf4
Rafa07C <167788140+Rafa07C@users.noreply.github.com>
2026-02-28 14:53:18 -0600

    chore: add .NET 6 solution with API and MVC projects

---
+++ src/CreditCardStatement.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CreditCardStatement.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
